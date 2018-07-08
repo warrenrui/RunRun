0f5292079fe9257a53f9ad76c23117cc45e47f36
warrenrui <crazymalao@163.com>
2018-07-08 22:54:04 +0800

    新建Price类

---
+++ src/refactoring/rentmovie/Price.java
@@ -0,0 +1,29 @@
+package refactoring.rentmovie;
+
+public abstract class Price {
+    abstract int getPriceCode();
+}
+
+class ChildrenPrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDREN;
+    }
+}
+
+class NewReleasePrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
