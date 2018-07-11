96699e3669c012d9eba841961a403d3749dcfdbd
warrenrui <crazymalao@163.com>
2018-07-11 22:30:38 +0800

    手动移动方法

--- src/refactoring/rentmovie/Movie.java
+++ src/refactoring/rentmovie/Movie.java
@@ -6,7 +6,6 @@ public class Movie {
     static final int CHILDREN = 2;
 
     private String title;
-    private int priceCode;
     private Price price;
 
     public Movie(String title, int priceCode) {
@@ -27,27 +26,7 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double charge = 0;
-        // determine amounts for each line
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                charge += 2;
-                if (daysRented > 2) {
-                    charge += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                charge += 1.5;
-                if (daysRented > 3) {
-                    charge += (daysRented - 3) * 1.5;
-                }
-                break;
-            default:
-        }
-        return charge;
+        return price.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints(int daysRented) {
