9f0d99f5717742ab137eef7f49f05023cf179557
warrenrui <crazymalao@163.com>
2018-11-21 10:22:47 +0800

    Apache commons 工具包

---
+++ src/commonslang/CommonsLang.java
@@ -0,0 +1,19 @@
+package commonslang;
+
+/**
+ * Apache工具包
+ *
+ * <dependency>
+ * <groupId>org.apache.commons</groupId>
+ * <artifactId>commons-lang3</artifactId>
+ * <version>3.8</version>
+ * </dependency>
+ */
+public class CommonsLang {
+    public static void main(String[] args) {
+//        ArrayUtils
+        /**
+         * 使用commons-lang3自带的Junit看更好
+         */
+    }
+}
