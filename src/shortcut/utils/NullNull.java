408a32720b45fe78e53ee60edbe0901684e7b8bc
warrenrui <crazymalao@163.com>
2019-05-21 11:26:44 +0800

    用equals方法比较字符串时，可能为null的放后面

--- src/shortcut/utils/NullNull.java
+++ src/shortcut/utils/NullNull.java
@@ -1,12 +1,17 @@
 package shortcut.utils;
 
 
-
 import java.util.HashMap;
 
 public class NullNull {
     public static void main(String[] args) {
         final HashMap<String, Object> map = new HashMap<>();
+        /**
+         * 用equals方法比较字符串时，可能为null的放后面
+         */
+        System.out.println("abc".equals(map.get("abc")));
+        // NullPointerException
+//        System.out.println(map.get("abc").equals("abc"));
         // null
         System.out.println(map.get("abc"));
         // null
@@ -16,8 +21,6 @@ public class NullNull {
         System.out.println(abc);
         // NullPointerException
         System.out.println(String.valueOf(null));
-
-
 
 
     }
