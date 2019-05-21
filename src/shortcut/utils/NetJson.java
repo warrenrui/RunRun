b7c95e16d04398e88d244b017c66b28aae33c273
warrenrui <crazymalao@163.com>
2019-05-21 11:23:51 +0800

    解析字符串之前，回车换行字符替换

--- src/shortcut/utils/NetJson.java
+++ src/shortcut/utils/NetJson.java
@@ -9,6 +9,16 @@ public class NetJson {
 
 
     public static void main(String[] args) {
+
+        String jsonStr = "{\"orgNo\":\"712\",\"distributeNo\":\"729\n" +
+                "\",\"warehouseNo\":\"632\",\"businessId\":\"BackOmc\",}";
+        /**
+         * 对于字符串中包含\n【表示换行，相当于按一下回车】，或者\t【表示空四个字符，也称缩进，相当于按一下Tab键】，替换成空字符
+         */
+        jsonStr = jsonStr.replace('\n', ' ').replace('\t', ' ').trim();
+        Map<String, String> map0 = (Map<String, String>) JSONObject.toBean(JSONObject.fromObject(jsonStr), Map.class);
+
+
         String json = "{\"skuId\":\"EMG4418057095224\",\"typeCode\":0}";
         Map<String, String> map = (Map<String, String>) JSONObject.toBean(JSONObject.fromObject(json), Map.class);
         Map<String, String> maps = com.alibaba.fastjson.JSONObject.parseObject(json, Map.class);
